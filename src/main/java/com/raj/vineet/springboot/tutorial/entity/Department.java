54a85cdf5de43508b3d2a64ca58be3a6f77b25b2
vineetraj <vineetraj14@gmail.com>
2022-09-11 22:19:15 +0530

    Add Lombok & remove boilerplate code in Department entity

--- src/main/java/com/raj/vineet/springboot/tutorial/entity/Department.java
+++ src/main/java/com/raj/vineet/springboot/tutorial/entity/Department.java
@@ -1,12 +1,21 @@
 package com.raj.vineet.springboot.tutorial.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
+@Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Department {
 
     @Id
@@ -17,56 +26,56 @@ public class Department {
     private String departmentAddress;
     private String departmentCode;
 
-    public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
-        this.departmentId = departmentId;
-        this.departmentName = departmentName;
-        this.departmentAddress = departmentAddress;
-        this.departmentCode = departmentCode;
-    }
-
-    public Department() {
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "departmentId=" + departmentId +
-                ", departmentName='" + departmentName + '\'' +
-                ", departmentAddress='" + departmentAddress + '\'' +
-                ", departmentCode='" + departmentCode + '\'' +
-                '}';
-    }
-
-    public Long getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(Long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentAddress() {
-        return departmentAddress;
-    }
-
-    public void setDepartmentAddress(String departmentAddress) {
-        this.departmentAddress = departmentAddress;
-    }
-
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
-
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
+//    public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
+//        this.departmentId = departmentId;
+//        this.departmentName = departmentName;
+//        this.departmentAddress = departmentAddress;
+//        this.departmentCode = departmentCode;
+//    }
+//
+//    public Department() {
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Department{" +
+//                "departmentId=" + departmentId +
+//                ", departmentName='" + departmentName + '\'' +
+//                ", departmentAddress='" + departmentAddress + '\'' +
+//                ", departmentCode='" + departmentCode + '\'' +
+//                '}';
+//    }
+//
+//    public Long getDepartmentId() {
+//        return departmentId;
+//    }
+//
+//    public void setDepartmentId(Long departmentId) {
+//        this.departmentId = departmentId;
+//    }
+//
+//    public String getDepartmentName() {
+//        return departmentName;
+//    }
+//
+//    public void setDepartmentName(String departmentName) {
+//        this.departmentName = departmentName;
+//    }
+//
+//    public String getDepartmentAddress() {
+//        return departmentAddress;
+//    }
+//
+//    public void setDepartmentAddress(String departmentAddress) {
+//        this.departmentAddress = departmentAddress;
+//    }
+//
+//    public String getDepartmentCode() {
+//        return departmentCode;
+//    }
+//
+//    public void setDepartmentCode(String departmentCode) {
+//        this.departmentCode = departmentCode;
+//    }
 
 }
